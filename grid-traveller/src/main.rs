dd773fce949d8d8762901fa6178d38fda264b648
Auggie <auggie@thebennett.net>
2024-08-19 13:20:59 -0500

    optization of grid-traveller

--- grid-traveller/src/main.rs
+++ grid-traveller/src/main.rs
@@ -1,8 +1,20 @@
 use std::collections::HashMap;
+use std::time::SystemTime;
 
 fn main() {
+    let now = SystemTime::now();
     let mut memo = HashMap::new();
     println!("{}", grid_traveler(18,18, &mut memo));
+    match now.elapsed() {
+        Ok(elapsed) => {
+            // it prints '2'
+            println!("{}", elapsed.subsec_nanos());
+        }
+        Err(e) => {
+            // an error occurred!
+            println!("Error: {e:?}");
+        }
+    }
 }
 
 
@@ -17,6 +29,11 @@ fn grid_traveler(x: i32, y: i32, memo: &mut HashMap<String, i128>) -> i128 {
             if let Some(&result) = memo.get(&search_key) {
                 return result;
             }
+            let search_key = format!("{},{}", y, x);
+            if let Some(&result) = memo.get(&search_key) {
+                return result;
+            }
+            
 
             let result = grid_traveler(x-1, y, memo) + grid_traveler(x, y-1, memo);
             memo.insert(search_key, result);
